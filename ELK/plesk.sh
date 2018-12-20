4c32065015ceb5dd1967c1b0c2f7d45281603e32
datpt <thanhdatkma96@gmail.com>
2018-12-20 09:30:28 +0700

    Update plesk.sh

--- ELK/plesk.sh
+++ ELK/plesk.sh
@@ -1,31 +1,19 @@
-
 #!/bin/bash
-# WHM/Cpanel change IP
+# Plesk change IP
 # CanhDX NhanHoa Cloud Team 
 
+new_ip=$(ip -4 addr show eth0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}' | head -n 1)
+
 # Get info
-NEW_IP=$(ip -4 addr show eth0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}' | head -n 1)
-OLD_IP=$(cat /usr/local/apache/conf/httpd.conf | grep VirtualHost  | head -n 1 | awk '{print $2}' | cut -d  ':' -f1)
-mysql_old_passwd=$(cat /root/.my.cnf | grep password | cut -d '=' -f2 |  tr -d '"')
+plesk bin reconfigurator /root/ip_map_file_name
 
-# Input from cloud-init
-mysql_new_passwd=$1
+iface=$(cat /root/ip_map_file_name | grep -e 'eth\|ens' | awk '{print $1}')
+old_ip=$(cat /root/ip_map_file_name | grep -e 'eth\|ens' | awk '{print $2}')
+netmask=$(cat /root/ip_map_file_name | grep -e 'eth\|ens' | awk '{print $3}')
+echo "$iface $old_ip $netmask -> $iface $new_ip $netmask" > /root/ip_map_file_name
 
-# Input from random 
-# mysql_new_passwd=$(date +%s | sha256sum | base64 | head -c 10 ; echo)
+# Reconfig
+plesk bin reconfigurator /root/ip_map_file_name
 
-# Sed info
-sed -Ei "s/$OLD_IP/$NEW_IP/g" /usr/local/apache/conf/httpd.conf
-sed -Ei "s/$OLD_IP/$NEW_IP/g" /etc/wwwacct.conf
-sed -Ei "s/$OLD_IP/$NEW_IP/g" /etc/hosts
-sed -Ei "s/$OLD_IP/$NEW_IP/g" /var/cpanel/mainip
-
-# Change MySQL root password 
-mysqladmin --user=root --password=$mysql_old_passwd password $mysql_new_passwd
-
-# Save mysql info 
-sed -Ei "s/$mysql_old_passwd/$mysql_new_passwd/g"  /root/.my.cnf 
-
-# Restart Service 
-service named restart || systemctl restart named
-service httpd restart || systemctl restart httpd
+# Remove file
+rm -f /root/ip_map_file_name
